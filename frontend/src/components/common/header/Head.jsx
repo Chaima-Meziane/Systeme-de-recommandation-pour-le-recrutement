f46474ff26c9fc12d8a4a73fd62c81e35b28f4ae
Aicha0 <aicha.benmessaoud@esprit.tn>
2023-08-25 18:38:40 +0200

    Adjust CSS styles in Hero component

--- frontend/src/components/common/header/Head.jsx
+++ frontend/src/components/common/header/Head.jsx
@@ -6,8 +6,8 @@ const Head = () => {
       <section className='head'>
         <div className='container flexSB'>
           <div className='logo'>
-            <h1>ACADEMIA</h1>
-            <span>ONLINE EDUCATION & LEARNING</span>
+            <h1>Esprit's Job Center</h1>
+            <span> ÉPANOUISSEMENT PROFESSIONNEL ASSURÉ</span>
           </div>
 
           <div className='social'>
