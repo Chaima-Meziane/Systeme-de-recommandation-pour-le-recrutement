21bae91d95ecc178afeffe4af61600a4f9b4ed8a
Aicha0 <aicha.benmessaoud@esprit.tn>
2023-08-26 20:58:58 +0200

    Update user management in Django admin interface

--- JobRecommendationSystem/account/admin.py
+++ JobRecommendationSystem/account/admin.py
@@ -1,4 +1,27 @@
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib import admin
 from .models import User
-# Register your models here.
-admin.site.register(User);
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {
+            'fields': ('is_candidat', 'is_coordinateur'),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Fields', {
+            'fields': ('is_candidat', 'is_coordinateur'),
+        }),
+    )
